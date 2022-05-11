bc01839436bc02a11e91bceb067bfc1ab41b952e
AniaPasyk <aniapasyk@gmail.com>
2022-05-11 08:47:29 +0200

    Revert "project clean up"
    This reverts commit 7762c8eeea5fef387b63bbad8b443f8f33a9e50c.

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,3 +1,12 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
 public class Login {
     @Test
     public void demoTest(){
